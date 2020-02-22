33d2457144692be8e931b75dee4499707593a550
helitonoliveira <heliton.oliveira88@gmail.com>
2020-02-21 22:48:54 -0300

    initial config backend nodejs

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World!' });
-});
+routes.get('/', (req, res) => res.json({ message: 'Hello World!' }));
 
 export default routes;
