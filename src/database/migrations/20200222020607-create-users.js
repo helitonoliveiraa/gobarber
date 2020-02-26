c322106371526a413982b7db99afc548f631c18a
helitonoliveira <heliton.oliveira88@gmail.com>
2020-02-25 22:54:40 -0300

    fix: migration create-user

--- src/database/migrations/20200222020607-create-users.js
+++ src/database/migrations/20200222020607-create-users.js
@@ -18,7 +18,7 @@ module.exports = {
       },
       password_hash: {
         type: Sequelize.STRING,
-        allowNull: true
+        allowNull: false
       },
       provider: {
         type: Sequelize.BOOLEAN,
