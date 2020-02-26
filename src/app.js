56efe35e4fbdcdfba2a42984c8e40ddc02e764bb
helitonoliveira <heliton.oliveira88@gmail.com>
2020-02-26 16:54:13 -0300

    feature: list of services providers

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
 // import sem retorno
@@ -14,6 +15,10 @@ class App {
 
   middleware() {
     this.server.use(express.json());
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
   }
 
   routes() {
