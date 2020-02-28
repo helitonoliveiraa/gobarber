e73c42ce4fb0da2174e646a62a11b4c14c52ed7f
helitonoliveira <heliton.oliveira88@gmail.com>
2020-02-28 00:47:50 -0300

    feature: scheduling functionality

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -20,6 +22,11 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', uploads.single('file'), FileController.store);
 
