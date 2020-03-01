59c254b4bb86eecae5b3964cac03590122447d61
helitonoliveira <heliton.oliveira88@gmail.com>
2020-03-01 12:29:16 -0300

    send notification

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -58,6 +60,16 @@ class AppointmentController {
         .json({ error: 'You can only create appointments with providers' });
     }
 
+    const checkUserIsProvider = await User.findOne({
+      where: { id: req.userId, provider: true }
+    });
+
+    if (checkUserIsProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Providers can not create an appointment' });
+    }
+
     /**
      * Check for past date
      */
@@ -84,6 +96,21 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date
+    });
+
+    /**
+     * Notify appointment provider
+     */
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locale: pt }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+      user: provider_id
     });
 
     return res.json(appointment);
--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -11,6 +12,7 @@ const models = [User, File, Appointment]; // todos os models da aplicação
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   // Este método init() faz a conexão com a base de dados
@@ -21,6 +23,17 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true
+      }
+    );
   }
 }
 
