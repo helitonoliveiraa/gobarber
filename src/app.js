30a1c7254b43c76f2512531b943b1bea2bb2b953
Heliton Oliveira <heliton.oliveira88@gmail.com>
2020-06-23 01:11:13 -0300

    setting docker

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
-const expess = require('express');
-const routes = require('./routes');
+import express from 'express';
+import routes from './routes';
 
 class App {
   constructor() {
-    this.server = expess();
+    this.server = express();
 
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
-    this.server.use(expess.json(routes));
+    this.server.use(express.json());
   }
 
   routes() {
-    this.server.use();
+    this.server.use(routes);
   }
 
 }
