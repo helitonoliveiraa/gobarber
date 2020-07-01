70f7f288997fea7bf29d715b383f8e3b31ea6a79
Heliton Oliveira <heliton.oliveira88@gmail.com>
2020-06-30 22:30:46 -0300

    refectory includes

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -3,6 +3,7 @@ import * as Yup from 'yup';
 
 import authConfig from '../../config/auth';
 import User from '../models/User';
+import File from '../models/File';
 
 class SessionController {
   async store(req, res) {
@@ -17,7 +18,16 @@ class SessionController {
 
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     if (!user) {
       return res.status(401).json({ error: 'User not found' });
@@ -27,13 +37,15 @@ class SessionController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar, provider } = user;
 
     return res.json({
       user: {
         id,
         name,
         email,
+        avatar,
+        provider,
       },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
