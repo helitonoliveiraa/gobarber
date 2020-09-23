048e4deb86c929cac651f2a8bc09af96b903840f
Héliton Oliveira <heliton.oliveira88@gmail.com>
2020-09-23 16:07:56 -0300

    :sparkles: feat: cache of providers

--- backend/src/app/controllers/ProviderController.js
+++ backend/src/app/controllers/ProviderController.js
@@ -1,9 +1,17 @@
 import User from '../models/User';
 import File from '../models/File';
 
+import Cache from '../../lib/Cache';
+
 class ProviderController {
   async index(req, res) {
-    const provider = await User.findAll({
+    const cached = await Cache.get('providers');
+
+    if (cached) {
+      return res.json(cached);
+    }
+
+    const providers = await User.findAll({
       where: { provider: true },
       attributes: ['id', 'name', 'email', 'avatar_id'],
       include: [
@@ -15,7 +23,9 @@ class ProviderController {
       ],
     });
 
-    return res.json(provider);
+    await Cache.set('providers', providers);
+
+    return res.json(providers);
   }
 }
 
